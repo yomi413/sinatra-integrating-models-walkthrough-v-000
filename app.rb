b7a9d03abbc7d563ab23e66dd944761ec19236e3
Yomaira Escano <yjescano@gmail.com>
2018-01-18 00:02:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,12 +2,13 @@ require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
+
   get '/' do
     erb :index
   end
 
   post '/' do
-    text_from_user = params[:user_text]
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,49 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  attr_reader :text
+
+  def initialize(text)
+    @text = text.downcase
+  end
+
+  def count_of_words
+    words = text.split(" ")
+    words.count
+  end
+
+  def count_of_vowels
+    text.scan(/[aeoui]/).count
+  end
+
+  def count_of_consonants
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  end
+
+  def most_used_letter
+    sl = text.gsub(/[^a-z]/, '') # gets rid of spaces
+    arr = sl.split('')
+    arr1 = arr.uniq
+    arr2 = {}
+
+    arr1.map do |c|
+      arr2[c] = arr.count(c)
+    end
+
+    biggest = { arr2.keys.first => arr2.values.first }
+
+    arr2.each do |key, value|
+      if value > biggest.values.first
+        biggest = {}
+        biggest[key] = value
+      end
+    end
+
+    biggest
+  end
+end
+
+# my_text = TextAnalyzer.new("The rain in Spain stays mainly on the plain.")
+# my_text.count_of_words #=> 9
+# my_text.count_of_vowels #=> 13
+# my_text.count_of_consonants #=> 22
+# my_text.most_used_letter #=> ["n", 6]
